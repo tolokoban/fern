e2a72dcdff0c5862d1c8601d720cee795d7ecccf
Fabien PETITJEAN <fabien.petitjean@epfl.ch>
2023-10-18 17:36:09 +0200

    Remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 
 import { makeGoto } from "@/utils/goto"
-import { useBarnsleyParams } from "@/data/data"
 
 export default function Page() {
     React.useEffect(makeGoto(`/barnsley/edit`), [])
